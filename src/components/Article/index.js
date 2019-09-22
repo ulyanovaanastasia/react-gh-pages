8f2b582f7f010d5c2f6e4854ed62d34c46dbb2c8
Ulyanova Anastasia <uljanova.anastasia@gmail.com>
2019-09-23 00:49:55 +0300

    accordeon

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -4,26 +4,16 @@ class Article extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			isOpen: this.props.defaultOpen
 		};
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick(e){
-		this.setState({
-			isOpen: !this.state.isOpen
-		});
-	}
-	componentWillReceiveProps(nextProps){
-		if(nextProps.defaultOpen!==this.props.defaultOpen) this.setState({isOpen: nextProps.defaultOpen});
 	}
 	render(){
-		const article = this.props.article;
-		const body = this.state.isOpen && <div className="article__text card-body">{article.text}</div>;
+		const {article, isOpen} = this.props;
+		const body = isOpen && <div className="article__text card-body">{article.text}</div>;
 		return(
 			<div className="article mb-4 card">
 				<div className="article__header card-header d-flex">
 					<h2 className="article__title">{article.title}</h2>
-					<div className="btn btn-primary ml-auto" onClick={this.handleClick}>{this.state.isOpen?'Close':'Open'}</div>
+					<div className="btn btn-primary ml-auto" onClick={this.props.onClick}>{isOpen?'Close':'Open'}</div>
 				</div>
 				<div className="card-body">
 					<div className="card-subtitle article__date text-gray">Date: {article.date}</div>
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -4,11 +4,18 @@ import Article from '../Article';
 export default class ArticleList extends React.PureComponent{
 	constructor(props){
 		super(props);
-
+		this.state = {
+			openArticleId: null
+		}
+	}
+	handleClick(openArticleId){
+		this.setState({
+			openArticleId: (this.state.openArticleId===openArticleId)?null:openArticleId
+		});
 	}
 	render(){
 		const articleList = this.props.articleList.map((article, index) => 
-			<Article key={article.id} article = {article} defaultOpen = {index===0}/>
+			<Article key={article.id} article = {article} isOpen={this.state.openArticleId===article.id} onClick = {this.handleClick.bind(this, article.id)}/>
 		);
 		return(
 			<div className="articles">
