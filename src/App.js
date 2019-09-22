5889c06f1628db89e222544cc06f78756940c786
Ulyanova Anastasia <uljanova.anastasia@gmail.com>
2019-09-22 21:02:16 +0300

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import logo from './logo.svg';
 import './App.sass';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App d-flex flex-column h-100">
+			<div className="header"></div>
+			<div className="main flex-grow-1">
+				<div className="container">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						My app 3
+					</p>
+					<button className="btn btn-primary">button</button>
+				</div>
+			</div>
+			<div className="footer"></div>
+		</div>
+	);
 }
 
 export default App;
